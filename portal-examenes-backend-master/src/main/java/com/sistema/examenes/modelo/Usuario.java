cf6038f8a76acec3f78400ff1ecdb28c39b80e78
x1n4px <silosabia@gmail.com>
2023-03-16 23:36:11 +0100

    Mvn dependencies

--- portal-examenes-backend-master/src/main/java/com/sistema/examenes/modelo/Usuario.java
+++ portal-examenes-backend-master/src/main/java/com/sistema/examenes/modelo/Usuario.java
@@ -1,6 +1,10 @@
 package com.sistema.examenes.modelo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -8,7 +12,10 @@ import javax.persistence.*;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "usuarios")
 public class Usuario implements UserDetails {
@@ -16,11 +23,12 @@ public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(unique = true)
     private String username;
     private String password;
     private String nombre;
     private String apellido;
+    @Column(unique = true)
     private String email;
     private String telefono;
     private boolean enabled = true;
@@ -28,105 +36,22 @@ public class Usuario implements UserDetails {
     private String rolAsignado;
 
 
-    public Usuario() {
-
-    }
-
-    public String getRolAsignado() {
-        return rolAsignado;
-    }
-
-    public void setRolAsignado(String rolAsignado) {
-        this.rolAsignado = rolAsignado;
-    }
-
-    public Usuario(Long id, String username, String password, String nombre, String apellido, String email, String telefono, boolean enabled, String perfil) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
-        this.telefono = telefono;
-        this.enabled = enabled;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
 
     @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
+    public boolean isAccountNonExpired() {return true;}
 
-    public String getPassword() {
-        return password;
-    }
+    @Override
+    public boolean isAccountNonLocked() {return true;}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @Override
+    public boolean isCredentialsNonExpired() {return true;}
 
-    public String getNombre() {
-        return nombre;
-    }
+    public void setUsername(String username) {this.username = username;}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {return null;}
 
     public boolean isEnabled() {
         return enabled;
--- portal-examenes-backend-master/src/test/java/com/sistema/examenes/SistemaExamenesBackendApplicationTests.java
+++ portal-examenes-backend-master/src/test/java/com/sistema/examenes/SistemaExamenesBackendApplicationTests.java
@@ -1,13 +1,115 @@
 package com.sistema.examenes;
 
+import com.sistema.examenes.configuraciones.JwtUtils;
+import com.sistema.examenes.repositorios.UsuarioRepository;
+import io.jsonwebtoken.Claims;
+import jdk.jfr.internal.Utils;
+import lombok.var;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
 
-@SpringBootTest
+import javax.crypto.SecretKey;
+import java.net.URI;
+import java.util.Date;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("Test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class SistemaExamenesBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Value(value="${local.server.port}")
+	private int port;
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+
+	private JwtUtils jwtUtils;
+
+		@Mock
+		private SecretKey secretKey;
+
+
+
+
+
+		private URI uri(String scheme, String host, int port, String ...paths) {
+			UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+			UriBuilder ub = ubf.builder()
+					.scheme(scheme)
+					.host(host).port(port);
+			for (String path: paths) {
+				ub = ub.path(path);
+			}
+			return ub.build();
+		}
+
+		private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+			URI uri = uri(scheme, host,port, path);
+			var peticion = RequestEntity.get(uri)
+					.accept(MediaType.APPLICATION_JSON)
+					.build();
+			return peticion;
+		}
+
+		private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+			URI uri = uri(scheme, host,port, path);
+			var peticion = RequestEntity.delete(uri)
+					.build();
+			return peticion;
+		}
+
+		private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+			URI uri = uri(scheme, host,port, path);
+			var peticion = RequestEntity.post(uri)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(object);
+			return peticion;
+		}
+
+		private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+			URI uri = uri(scheme, host,port, path);
+			var peticion = RequestEntity.put(uri)
+					.contentType(MediaType.APPLICATION_JSON)
+					.body(object);
+			return peticion;
+		}
+
+
+
+
+
+
+
+	@Nested
+	@DisplayName("cuando la base de datos está vacía")
+	public class BaseDatosVacia {
+
+	}
+
+
+	@Nested
+	@DisplayName("Cuando la base de datos tiene información")
+	public class BaseDatosLlena{
+
 	}
 
 }
